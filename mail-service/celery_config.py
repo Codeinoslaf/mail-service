0261b6da6e7bb5077903644e6498fe28ffb0d6c5
Николай С <nicksrnk@gmail.com>
2025-06-03 15:01:01 +0500

    Cellery рабоатает, команда для запуска celery -A mailservice worker --loglevel=info

--- mail-service/celery_config.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mail_service.settings')
-
-app = Celery('mail_service')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
