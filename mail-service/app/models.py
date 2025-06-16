3c7b5a980b0fe78e27618830bbc1e33af4e69757
tima <tchernusho@gmail.com>
2025-06-16 15:30:14 +0500

    - добавил модель адресатов

--- mail-service/app/models.py
+++ mail-service/app/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 
+class Recipient(models.Model):
+    address = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = 'Получатель'
+        verbose_name_plural = 'Получатели'
+
+    def __str__(self):
+        return self.address
+
 class Status(models.Model):
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=200)
@@ -8,6 +18,9 @@ class Status(models.Model):
     class Meta:
         verbose_name = 'Статус'
         verbose_name_plural = 'Статусы'
+
+    def __str__(self):
+        return self.name
 
 
 class Task(models.Model):
@@ -19,25 +32,19 @@ class Task(models.Model):
         verbose_name = 'Задание'
         verbose_name_plural = 'Задания'
 
+    def __str__(self):
+        return f"{self.subject} от {self.created_at} "
+
 
 class Email(models.Model):
-    recipient_list = models.TextField(help_text="Введите почты получателей") # не список на самом деле
+    recipient_list = models.ManyToManyField(Recipient, related_name="recipients")
     send_at = models.DateTimeField(auto_now_add=True)
     status = models.ForeignKey(Status, null=True, blank=True, on_delete=models.SET_NULL)
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"Почтовое письмо отправлено на: {self.recipient_list} ---> {Status.name}"
+        return f"Почтовое письмо отправлено на: {[recipient.address + " " for recipient in self.recipient_list.all()]}"
 
     class Meta:
         verbose_name = 'Сообщение'
         verbose_name_plural = 'Сообщения'
-
-
-class Recipient(models.Model):
-    address = models.CharField(max_length=255)
-    tasks = models.ManyToManyField(Email, related_name="recipients")
-
-    class Meta:
-        verbose_name = 'Получатель'
-        verbose_name_plural = 'Получатели'
